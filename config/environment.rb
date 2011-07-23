cc4389d1116392db2e39f455e4a559616a69b95a
Jonathan Hernandez <ion@suavizado.com>
2011-07-22 19:03:41 -0600

    Google Apps config constants

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
+# Config
+GAPPS_DOMAIN = 'your-domain.com'
+GAPPS_CONSUMER_KEY = GAPPS_DOMAIN
+GAPPS_CONSUMER_SECRET = 'your-consumer-googleapps-key'
+GAPPS_REQUESTOR_ID = 'some.admin@your-domain.com'
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Sapos::Application.initialize!
+
