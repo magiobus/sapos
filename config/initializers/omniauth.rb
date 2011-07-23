5d773965f3a51b9d0bcefae43fd94c0e1cca8778
Jonathan Hernandez <ion@suavizado.com>
2011-07-22 19:06:43 -0600

    Use config constant

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,6 @@
 require 'openid/store/filesystem'
 
-# Set the default hostname for omniauth to send callbacks to.
-# OmniAuth.config.full_host = "http://posgrado.cimav.edu.mx:80"
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => 'cimav.edu.mx', :name => 'admin'
+  provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => GAPPS_DOMAIN, :name => 'admin'
 end
 
