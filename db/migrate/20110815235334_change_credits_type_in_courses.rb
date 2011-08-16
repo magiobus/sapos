1ce699f257931ea853bd644a46881d49b87fa110
Jonathan Hernandez <ion@suavizado.com>
2011-08-15 18:00:54 -0600

    Change credits type

--- db/migrate/20110815235334_change_credits_type_in_courses.rb
+++ db/migrate/20110815235334_change_credits_type_in_courses.rb
@@ -1,9 +1,13 @@
 class ChangeCreditsTypeInCourses < ActiveRecord::Migration
   def self.up
     change_column :courses, :credits, :decimal, :precision => 8, :scale => 2
+    change_column :courses, :lecture_hours, :decimal, :precision => 8, :scale => 2
+    change_column :courses, :lab_hours, :decimal, :precision => 8, :scale => 2
   end
 
   def self.down
     change_column :courses, :credits, :integer
+    change_column :courses, :lecture_hours, :integer
+    change_column :courses, :lab_hours, :integer
   end
 end
