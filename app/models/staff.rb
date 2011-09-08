c8cf612401148556c460ccea001f189e7ec7b29c
Jonathan Hernandez <ion@suavizado.com>
2011-09-08 17:30:20 -0600

    Validates institution_id

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -8,6 +8,7 @@ class Staff < ActiveRecord::Base
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
+  validates :institution_id, :presence => true
   
   after_create :add_extra
 
