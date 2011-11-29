e675951973fa3289608c016c01e5b9a7db5b0cd2
Jonathan Hernández <ion@suavizado.com>
2011-11-29 16:33:29 -0700

    Update end_date with defence_date

--- app/models/thesis.rb
+++ app/models/thesis.rb
@@ -1,6 +1,19 @@
 # coding: utf-8
 class Thesis < ActiveRecord::Base
   belongs_to :student
+  after_update :set_student_status
+
+  CONCLUDED   = 'C'
+  IN_PROGRESS = 'P'
+  INACTIVE    = 'I'
+
+  STATUS = {CONCLUDED   => 'Concluida',
+            IN_PROGRESS => 'En progreso',
+            INACTIVE    => 'Inactiva'}
+
+  def status_type
+    STATUS[status]
+  end
 
   def set_number
     #TODO Add validations
@@ -21,6 +34,15 @@ class Thesis < ActiveRecord::Base
     self.consecutive = con
     self.number = "#{self.student.program.prefix}#{consecutive}"
     self.save
+  end
+
+  def set_student_status
+    if self.status == CONCLUDED
+      s = Student.find(self.student_id)
+      s.end_date = self.defence_date
+      s.status = Student::GRADUATED
+      s.save
+    end
   end
 
 end
